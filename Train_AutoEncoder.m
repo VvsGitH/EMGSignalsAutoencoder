c6978b814434be0e68d978915c89bfbbc35c167d
Vito Paparella S <vs.paparella@gmail.com>
2020-02-04 13:08:10 +0100

    new training function

--- Train_AutoEncoder.m
+++ Train_AutoEncoder.m
@@ -28,7 +28,7 @@ emgToForceMatrix = cell(1,10);
 parfor h = 1:10
     
     fprintf('H%d: Generating Net...\n',h);
-    net = netAutoEncoder(h, EMG_Train, 1000, 1e-05, 0);
+    net = netAutoEncoder(h, EMG_Train, 5000, 1e-05, 0);
     
     %% TRAINING
     fprintf('H%d: Training...\n',h);
@@ -74,16 +74,16 @@ end
 
 %% SAVING
 fprintf('Generating Structure...\n');
-AEsim.subject = trSogg;
-AEsim.trainedNet(trSogg,:) = trainedNet;
-AEsim.trainingReport(trSogg,:) = trainingReport;
-AEsim.emgToForceMatrix(trSogg,:) = emgToForceMatrix;
-AEsim.MSE_emg(trSogg,:) = MSE_emg;
-AEsim.MSE_frc(trSogg,:) = MSE_frc;
-AEsim.RMSE_emg(trSogg,:) = RMSE_emg;
-AEsim.RMSE_frc(trSogg,:) = RMSE_frc;
-AEsim.R2_emg(trSogg,:) = R2_emg;
-AEsim.R2_frc(trSogg,:) = R2_frc;
+AEsim.subject = trSogg';
+AEsim.trainedNet = trainedNet';
+AEsim.trainingReport = trainingReport';
+AEsim.emgToForceMatrix = emgToForceMatrix';
+AEsim.MSE_emg = MSE_emg';
+AEsim.MSE_frc = MSE_frc';
+AEsim.RMSE_emg = RMSE_emg';
+AEsim.RMSE_frc = RMSE_frc';
+AEsim.R2_emg = R2_emg';
+AEsim.R2_frc = R2_frc';
 
 if (input('Save the file? [Y,N]\n','s') == 'Y')
     fprintf('Saving...\n');
@@ -96,17 +96,17 @@ fprintf('Plotting Performance Indexes...\n')
 h = 1:10;
 figure(1);
     subplot(2,3,1)
-    plot(h,AEsim.MSE_emg(AEsim.subject,:)), title('EMG MSE');
+    plot(h,AEsim.MSE_emg), title('EMG MSE');
     subplot(2,3,2)
-    plot(h,AEsim.RMSE_emg(AEsim.subject,:)), title('EMG RMSE');
+    plot(h,AEsim.RMSE_emg), title('EMG RMSE');
     subplot(2,3,3)
-    plot(h,AEsim.R2_emg(AEsim.subject,:)), title('EMG R2');
+    plot(h,AEsim.R2_emg), title('EMG R2');
     subplot(2,3,4)
-    plot(h,AEsim.MSE_frc(AEsim.subject,:)), title('FORCE MSE');
+    plot(h,AEsim.MSE_frc), title('FORCE MSE');
     subplot(2,3,5)
-    plot(h,AEsim.RMSE_frc(AEsim.subject,:)), title('FORCE RMSE');
+    plot(h,AEsim.RMSE_frc), title('FORCE RMSE');
     subplot(2,3,6)
-    plot(h,AEsim.R2_frc(AEsim.subject,:)), title('FORCE R2');    
+    plot(h,AEsim.R2_frc), title('FORCE R2');    
 
 fprintf('Plotting Signals...\n')
 t1 = 1:1:size(EMG_Test,2);
@@ -124,13 +124,13 @@ for h = 1:10
     sgtitle(['H' num2str(h) ': EMG'])
     
     % Estimating FORCE_Recos and plotting FORCE signals
-    inputWeigths = cell2mat(AEsim.trainedNet{AEsim.subject,h}.IW);
+    inputWeigths = cell2mat(AEsim.trainedNet{h}.IW);
     S_Test = elliotsig(inputWeigths*EMG_Test);
-    FORCE_Recos = AEsim.emgToForceMatrix{trSogg,h}*S_Test;
+    FORCE_Recos = AEsim.emgToForceMatrix{h}*S_Test;
     figure(2*h+1)
     for i = 1:6
         subplot(2,3,i)
-        plot(t1,FORCE_Test(i,:),'b');
+        plot(t1,FORCE_Test_den(i,:),'b');
         hold on
         plot(t2,FORCE_Recos(i,:),'r');
     end
--- netAutoEncoder.m
+++ netAutoEncoder.m
@@ -5,8 +5,10 @@ net = feedforwardnet(hiddenSize);
 net.biasConnect = [0;1];    % Il layer d'uscita EMG ha un bias
 
 % Set net functions
-net.trainFcn = 'trainscg'; %'trainlm': Jacobian - not supported by GPU; % 'trainscg': Scalar Conjugate Gradient - better for GPU
+net.trainFcn = 'trainlm'; %'traingda': Gradient Descent with adaptive learning rate %'trainlm': Jacobian derivatives - not supported by GPU; % 'trainscg': Scalar Conjugate Gradient - better for GPU
 net.performFcn = 'mse'; % Mean Square Error
+net.performParam.regularization = 0; % Minimize only error
+net.performParam.normalization = 'none'; % Take the error as it is
 net.divideFcn = 'dividetrain'; % Assegna tutti i valori al train
 net.layers{1}.transferFcn = 'poslin'; %'elliotsig' = n / (1 + abs(n)) - better for GPU; 'tansig' = 2/(1+exp(-2*n))-1
 net.layers{2}.transferFcn = 'purelin';
